9532d6e5304e8aa65e011b809ca8eb8048482db6
sh0tzz <lubranier@gmail.com>
2023-02-04 11:40:04 +0100

    .

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-// import { motion } from 'framer-motion';
 import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import Search from '../components/Search';
